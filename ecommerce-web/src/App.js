7bde927199860482ef0cbbe964ae2b8f24d94122
JakariaJishan <jakariajishan987@gmail.com>
2021-11-21 01:20:15 +0600

    ecommerce

---
+++ ecommerce-web/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hello world</h1>
+        <p>hi</p>
+        <h1>hello jack</h1>
+        <p>okh :bowtie:</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
