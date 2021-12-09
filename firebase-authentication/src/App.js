7497a4c4e5c610cec374fbf178e2db9fc58e4c8b
JakariaJishan <jakariajishan987@gmail.com>
2021-12-09 12:55:01 +0600

    password auth

--- firebase-authentication/src/App.js
+++ firebase-authentication/src/App.js
@@ -1,16 +1,20 @@
 import { initializeApp } from "firebase/app";
 import "firebase/auth";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from "react";
 import "./App.css";
 import firebaseConfig from "./firebase.config";
 
 function App() {
+  const [newUser, setNewUser] = useState(false);
   const [user, setUser] = useState({
     isSignIn: false,
     name: "",
     email: "",
-    photo: ""
+    password: '',
+    photo: "",
+    error: '',
+    success: false
   });
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
@@ -44,6 +48,79 @@ function App() {
     });
   }
 
+// email authentication action
+
+ let handleChange =(e) => {
+   let isValid;
+  if(e.target.name ==='name'){
+    isValid = true;
+  }
+   if(e.target.name === 'email'){
+    isValid = /\S+@\S+\.\S+/.test(e.target.value);
+   }
+   if ( e.target.name ==='password'){
+    isValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/.test(e.target.value);
+   }
+   
+   if(isValid){
+     let newValidation = {...user};
+     newValidation[e.target.name] = e.target.value;
+     setUser(newValidation);
+   }else{
+     console.log('kisuna')
+   }
+ }
+
+let handleSubmit = (e) =>{
+  e.preventDefault();
+  if (newUser && user.name && user.email && user.password){
+    const auth = getAuth();
+    createUserWithEmailAndPassword (auth,user.email, user.password)
+    .then((res) => {
+      let newUserInfo = {...user};
+      newUserInfo.error = '';
+      newUserInfo.success = true;
+      setUser(newUserInfo);
+    // Signed in 
+    // const user = userCredential.user;
+    // // ...
+    // console.log(res);
+  })
+  .catch((error) => {
+    let newUserInfo = {...user};
+    newUserInfo.error = error.message;
+    setUser(newUserInfo);
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // console.log(errorCode, errorMessage);
+  });
+  }
+
+
+  if (newUser && user.name && user.email && user.password){
+    const auth = getAuth();
+    signInWithEmailAndPassword (auth,user.email, user.password)
+    .then((res) => {
+      let newUserInfo = {...user};
+      newUserInfo.error = '';
+      newUserInfo.success = true;
+      setUser(newUserInfo);
+    // Signed in 
+    // const user = userCredential.user;
+    // // ...
+    // console.log(res);
+  })
+  .catch((error) => {
+    let newUserInfo = {...user};
+    newUserInfo.error = error.message;
+    setUser(newUserInfo);
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // console.log(errorCode, errorMessage);
+  });
+  }
+}
+
   return (
     <div className="App">
 
@@ -60,6 +137,23 @@ function App() {
           <img src={user.photo} alt=""></img>
         </div>
       )}
+
+      {/* email authentication */}
+
+      <h1>my own authentication</h1>
+      <input type="checkbox" name="newUser" onChange={() => setNewUser(!newUser)} /> <label>sign up</label>
+      <form onSubmit={handleSubmit}>
+        {newUser && <input type="text" name="name" onBlur={handleChange}  placeholder='your name'></input>}
+        <br/>
+        <input type="email" placeholder="enter your email" name="email" required onBlur={handleChange}></input>
+        <br/>
+        <input type="password" name="password" placeholder='password' required onBlur={handleChange}></input>
+        <br/>
+        <input type="submit" value="submit"></input>
+
+      </form>
+      <p style={{color: 'red'}}>{user.error}</p>
+      {user.success && <p style={{color: 'green'}}>user {newUser? 'created' : 'logged in'} successfully</p>}
     </div>
   );
 }
