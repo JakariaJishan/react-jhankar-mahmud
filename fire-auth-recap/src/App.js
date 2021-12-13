39e99b265d462cb508c3c5411000693dc60cf8e0
JakariaJishan <jakariajishan987@gmail.com>
2021-12-13 16:39:14 +0600

    private route failed working

--- fire-auth-recap/src/App.js
+++ fire-auth-recap/src/App.js
@@ -1,18 +1,34 @@
-import {
-  BrowserRouter, Route, Routes
-} from "react-router-dom";
+import { createContext, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/About/About";
 import Home from "./components/Home/Home";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import SignIn from "./components/SignIn/SignIn";
+
+export const categoryContext = createContext();
 
 function App() {
+  const [loggedinUser, setLoggedinUser] = useState({});
   return (
-    <BrowserRouter>
-      <div>
+    <categoryContext.Provider value={[loggedinUser, setLoggedinUser]}>
+      <h1>name: {loggedinUser.name}</h1>
+      <BrowserRouter>
         <h1>Hello, React Router!</h1>
+
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/signin" element={<SignIn />} />
+          <Route
+            path="/about"
+            element={
+              <PrivateRoute>
+                <About />
+              </PrivateRoute>
+            }
+          />
         </Routes>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </categoryContext.Provider>
   );
 }
 
--- fire-auth-recap/src/components/Home/Home.js
+++ fire-auth-recap/src/components/Home/Home.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
 const Home = () => {
-    const handleClick = () => {
-        console.log('clicked');
-    }
     return (
         <div>
             <h1>this is home page</h1>
-            <button onClick={handleClick}>click here</button>
         </div>
     );
 };
